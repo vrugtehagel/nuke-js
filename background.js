54f8730575b74242aea4a3b8afdef2e5ed85e47f
vrugtehagel <vrugtehagel@pm.me>
2024-08-31 12:26:01 +0200

    Add CSP to avoid inline scripts

--- background.js
+++ background.js
@@ -1,67 +1,74 @@
 browser.action.onClicked.addListener(async () => {
 	const query = {active: true, currentWindow: true}
 	const [activeTab] = await browser.tabs.query(query)
-	const url = new URL(activeTab.url)
-	const { origin } = url
-	if(cache.has(origin)) unnuke(origin)
+	const {origin} = new URL(activeTab.url)
+	const restore = cache.get(origin)
+	if(restore) restore()
 	else nuke(origin)
-	updateIcon(origin)
+	updateIcon(activeTab)
 })
 
+// Map an origin to a "restore" function. If this map includes an origin, then
+// it is nuked. To un-nuke, run the restore function.
 const cache = new Map()
-
-const createHandler = origin => event => {
-	const originUrl = new URL(event.originUrl)
-	if(originUrl.origin != origin) return
-	return { cancel: true }
-}
 
 async function nuke(origin){
 	if(cache.has(origin)) return
-	const handler = createHandler(origin)
-	browser.webRequest.onBeforeRequest.addListener(handler, {
-		types: ['script'],
-		urls: ['<all_urls>']
-	}, ['blocking'])
-	cache.set(origin, handler)
+	const handler = event => {
+		const name = 'Content-Security-Policy'
+		const value = 'script-src \'none\';'
+		event.responseHeaders.push({name, value})
+		return {responseHeaders: event.responseHeaders}
+	}
+	browser.webRequest.onHeadersReceived.addListener(handler, {
+		types: ['main_frame'],
+		urls: [`${origin}/*`]
+	}, ['blocking', 'responseHeaders'])
+	const restore = () => {
+		browser.webRequest.onHeadersReceived.removeListener(handler)
+		cache.delete(origin)
+		browser.storage.local.remove(origin)
+	}
+	cache.set(origin, restore)
+	browser.storage.local.set({[origin]: true})
 	const { hostname } = new URL(origin)
 	await browser.browsingData.remove({
 		hostnames: [hostname]
 	}, {cookies: true, cache: true})
-	const tabs = await browser.tabs.query({ url: `${origin}/*` })
+	const tabs = await browser.tabs.query({url: `${origin}/*`})
 	for(const tab of tabs) browser.tabs.reload(tab.id)
-	await browser.storage.local.set({[origin]: true})
 }
 
-async function unnuke(origin){
-	if(!cache.has(origin)) return
-	const handler = cache.get(origin)
-	browser.webRequest.onBeforeRequest.removeListener(handler)
-	cache.delete(origin)
-	await browser.storage.local.remove(origin)
-}
+// Block scripts requested by a nuked origin
+browser.webRequest.onBeforeRequest.addListener(event => {
+	const originUrl = new URL(event.originUrl)
+	if(cache.has(originUrl.origin)) return {cancel: true}
+}, {types: ['script'], urls: ['<all_urls>']}, ['blocking'])
 
+// When this script loads, restore the user's previous nukes
 browser.storage.local.get().then(data => {
 	const origins = Object.keys(data)
 	for(const origin of origins) nuke(origin)
 })
 
-function updateIcon(origin){
+// Update the icon whenever the user navigates around
+function updateIcon(tab){
+	if(!tab.url) return
+	const {origin} = new URL(tab.url)
+	const windowId = tab.windowId
 	const nuked = cache.has(origin)
 	const path = nuked ? 'nuked.svg' : 'icon.svg'
 	const title = nuked ? 'Nuked!' : 'Nuke it!'
-	browser.action.setIcon({path})
+	browser.action.setIcon({path, windowId})
 	browser.action.setTitle({title})
 }
 
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
 	if(!tab.active) return
-	const url = new URL(tab.url)
-	updateIcon(url.origin)
+	updateIcon(tab)
 }, {properties: ['url', 'status']})
 
 browser.tabs.onActivated.addListener(async ({tabId}) => {
 	const tab = await browser.tabs.get(tabId)
-	const url = new URL(tab.url)
-	updateIcon(url.origin)
+	updateIcon(tab)
 })
